d0fe144da1b2e7b436ebefb30ad79b1e85123206
Mateusz Szelagowski <mateoszzz@onet.pl>
2024-04-02 14:49:15 +0200

    Dodanie linku do resetowania hasła na stronie logowania

--- HW_Blog/settings.py
+++ HW_Blog/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = BASE_DIR / 'emails'
+EMAIL_FILE_PATH = BASE_DIR / 'Emails'
