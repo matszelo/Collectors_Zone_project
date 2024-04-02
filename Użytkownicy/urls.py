1afb355da383d147b0eba3300a10941fdc61ab0a
Mateusz Szelagowski <mateoszzz@onet.pl>
2024-04-02 14:02:20 +0200

    Dodanie możliwości resetowania hasła, gdy użytkownik nie może się zalogować na konto

--- Użytkownicy/urls.py
+++ Użytkownicy/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 app_name = 'Użytkownicy'
 urlpatterns = [
@@ -10,5 +11,10 @@ urlpatterns = [
     path('register_user', views.register_user, name='register_user'),
     path('user_profile/<int:pk>', views.user_profile, name='user_profile'),
     path('update_profile/<int:pk>', views.update_profile, name='update_profile'),
-    path('update_password/', views.update_password, name='update_password')
+    path('update_password/', views.update_password, name='update_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_sent'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
