2cc46f018f4fe6ab30d502e04121a9dd8a716745
Mateusz Szelagowski <mateoszzz@onet.pl>
2024-04-15 16:34:51 +0200

    Dodanie zdjęć profilowych użytkowników Forum obok komentarza

--- Użytkownicy/models.py
+++ Użytkownicy/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_pic = models.ImageField(null=True, blank=True, upload_to='profile_images/')
     bio = models.CharField(null=True, blank=True, max_length=255)
 
