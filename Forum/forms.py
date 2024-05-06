c3bc9ec8390fa5632a7ca601f5a59c00dcad93d7
Mateusz Szelagowski <mateoszzz@onet.pl>
2024-05-06 15:12:51 +0200

    Zmiana wyglądu w zakładkach dropu, postu oraz forum.

--- Forum/forms.py
+++ Forum/forms.py
@@ -8,6 +8,12 @@ class TopicForm(ModelForm):
         model = Temat
         fields = ('Tytuł', 'Kategoria', 'Opis')
 
+        widgets = {
+            'Tytuł': forms.TextInput(attrs={'class': 'form-control'}),
+            'Kategoria': forms.Select(attrs={'class': 'form-control'}),
+            'Opis': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
 class CommentForm(ModelForm):
     Tekst = forms.CharField(required=True, widget=forms.Textarea(
         attrs={
