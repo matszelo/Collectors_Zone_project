e7507b8c4f93f4aebbefa74ec0202e4ca45c98b8
Mateusz Szelagowski <mateoszzz@onet.pl>
2024-05-10 18:55:12 +0200

    Poprawiono wyszukiwanie dal niezalogowanych użytkowników.

--- Collectors_Zone/views.py
+++ Collectors_Zone/views.py
@@ -14,16 +14,32 @@ def home(request):
 
 
 def search(request):
-    if request.method == 'POST':
-        searched = request.POST['searched']
-        posts = Post.objects.filter(Q(Tytuł__icontains=searched) | Q(Opis__icontains=searched))
-        drops = Drop.objects.filter(Q(Tytuł__icontains=searched) | Q(Data__icontains=searched))
-        topics = Temat.objects.filter(Tytuł__icontains=searched)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            searched = request.POST['searched']
+            posts = Post.objects.filter(Q(Tytuł__icontains=searched) | Q(Opis__icontains=searched))
+            drops = Drop.objects.filter(Q(Tytuł__icontains=searched) | Q(Data__icontains=searched))
+            topics = Temat.objects.filter(Tytuł__icontains=searched)
 
-        if not posts and not drops and not topics:
-            messages.success(request, 'Brak wyszukiwań dla podanej frazy.')
-            return render(request, 'Main_page/search.html', {})
+            if not posts and not drops and not topics:
+                messages.success(request, 'Brak wyszukiwań dla podanej frazy.')
+                return render(request, 'Main_page/search.html', {})
+            else:
+                return render(request, 'Main_page/search.html',
+                              {'searched': searched, 'posts': posts, 'drops': drops, 'topics': topics})
         else:
-            return render(request, 'Main_page/search.html', {'searched': searched, 'posts': posts, 'drops': drops, 'topics': topics})
+            return render(request, 'Main_page/search.html', {})
+
     else:
-        return render(request, 'Main_page/search.html', {})
+        if request.method == 'POST':
+            searched = request.POST['searched']
+            posts = Post.objects.filter(Q(Tytuł__icontains=searched) | Q(Opis__icontains=searched))
+            drops = Drop.objects.filter(Q(Tytuł__icontains=searched) | Q(Data__icontains=searched))
+
+            if not posts and not drops:
+                messages.success(request, 'Brak wyszukiwań dla podanej frazy.')
+                return render(request, 'Main_page/search.html', {})
+            else:
+                return render(request, 'Main_page/search.html', {'searched': searched, 'posts': posts, 'drops': drops})
+        else:
+            return render(request, 'Main_page/search.html', {})
